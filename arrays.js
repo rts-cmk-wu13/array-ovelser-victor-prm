ebbee0612bbf7745c48c5056e8e38f5bf6fc936e
victor-prm <victorpabb@gmail.com>
2025-02-03 08:53:34 +0100

    solved task 6

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
@@ -14,25 +14,31 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Task 2:", catsArray);
 
     //Task 3a
-    function checkForDog(dogName){ return dogsArray.includes(dogName)};
+    function checkForDog(dogName) { return dogsArray.includes(dogName) };
     console.log("Task 3a:", checkForDog("Trixie"));
 
     //Task 3b
     console.log("Task 3b:", checkForDog("Baxter"));
 
     //Task 4a
-    function checkForCat(catName){ return catsArray.includes(catName)? true : undefined};
+    function checkForCat(catName) { return catsArray.includes(catName) ? true : undefined };
     console.log("Task 4a:", checkForCat("Bagheera"));
 
     //Task 4b
-    function checkForCat(catName){ return catsArray.includes(catName)? true : undefined};
+    function checkForCat(catName) { return catsArray.includes(catName) ? true : undefined };
     console.log("Task 4b:", checkForCat("Salem"));
 
     // Task 5
     let numberArrayTimesThree = [];
     numbersArray.forEach(number => {
-        numberArrayTimesThree.push(number*3)
+        numberArrayTimesThree.push(number * 3)
     });
     console.log("Task 5:", numbersArray, numberArrayTimesThree);
 
+    //Task 6
+    let dogsInOneString = "";
+    dogsArray.forEach(dog => {
+        dogsInOneString += dog;
+    })
+    console.log("Task 6:", dogsInOneString);
 }) // ends DOMContentLoaded
