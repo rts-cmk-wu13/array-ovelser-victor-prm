035358a82827a98544463b29de835ff9d1e8f726
victor-prm <victorpabb@gmail.com>
2025-02-03 08:36:31 +0100

    solved task 1

--- arrays.js
+++ arrays.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
-    // --- Løs opgaverne herunder --- 
+    // --- Løs opgaverne herunder ---
+    // Task 1
+    numbersLargerThanTwenty = numbersArray.filter((number) => number > 20);
+    console.log("Task 1:", numbersLargerThanTwenty)
 
 }) // ends DOMContentLoaded
